d3ac5832e79a86fea165672d0a51251aa407cb4f
lmossman <lake@airbyte.io>
2023-05-24 19:06:44 -0700

    object/array example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from "react-hook-form";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object({
+  objectField: yup
+    .object()
+    .shape({
+      requiredField: yup.string().required(),
+      optionalField: yup.string(),
+    })
+    .notRequired()
+    .default(undefined),
+  arrayField: yup
+    .array()
+    .of(
+      yup.object().shape({
+        requiredArrayField: yup.string().required(),
+        optionalArrayField: yup.string(),
+      })
+    )
+    .notRequired()
+    .default(undefined),
+});
 
 function App() {
+  const {
+    handleSubmit,
+    register,
+    setValue,
+    getValues,
+    formState: { errors },
+  } = useForm({
+    mode: "onChange",
+    resolver: yupResolver(schema),
+  });
+  console.log(errors);
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+        width: "400px",
+      }}
+    >
+      <button
+        type="button"
+        onClick={() =>
+          setValue(
+            "objectField",
+            { optionalField: "test" },
+            { shouldValidate: true }
+          )
+        }
+      >
+        Set object field to invalid value
+      </button>
+      <button
+        type="button"
+        onClick={() =>
+          setValue("objectField", undefined, { shouldValidate: true })
+        }
+      >
+        Clear object field
+      </button>
+      <button
+        type="button"
+        onClick={() =>
+          setValue("arrayField", [{ optionalArrayField: "test" }], {
+            shouldValidate: true,
+          })
+        }
+      >
+        Set array field to invalid value
+      </button>
+      <button
+        type="button"
+        onClick={() =>
+          setValue("arrayField", undefined, { shouldValidate: true })
+        }
+      >
+        Clear array field
+      </button>
+      <pre>{JSON.stringify(getValues(), null, 2)}</pre>
+    </form>
   );
 }
 
